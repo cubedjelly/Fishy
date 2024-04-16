16a352451b63ac0ac06a30b70e4d8acda53d52eb
cubedjelly <166545003+cubedjelly@users.noreply.github.com>
2024-04-16 17:01:39 -0400

    Fishing Bar Layout

---
+++ Assets/Fish_Bar_Minigame.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish_Bar_Minigame : MonoBehaviour
+{
+    [SerializeField] Transform TopPivot;
+    [SerializeField] Transform BottomPivot;
+
+    [SerializeField] Transform fish;
+   
+}
